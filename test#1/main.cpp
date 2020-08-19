4e62dd920f455b87e60b3d2d39f002accc58d06d
WildCard65 <lollol222gg@gmail.com>
2020-08-19 14:25:24 -0400

    Initial commit of the test.

---
+++ test#1/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Test 1 built without fail!" << std::endl;
+    return 0;
+}
---
+++ test#2/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Test 2 built without fail!" << std::endl;
+    std::cout << "The lobster king is: " << LOBSTER_KING << std::endl;
+    return 0;
+}
