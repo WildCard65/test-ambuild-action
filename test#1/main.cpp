54b261e6024b1d46d622e185d21b83455355c5fd
WildCard65 <lollol222gg@gmail.com>
2020-08-28 14:45:31 -0400

    Swapped test 1 and test 3

--- test#1/main.cpp
+++ test#1/main.cpp
@@ -1,18 +1,7 @@
 #include <iostream>
 
-void my_god_function(int my_value)
-{
-    std::cout << "WE GOT A WARNING!" << std::endl;
-}
-
-int my_ufunc() { return 52; }
-
 int main()
 {
-    std::cout << "Test 1 built without fail!" << '\n';
-    std::cout << "This program should've generated warnings." << '\n';
-
-    my_god_function(72);
-
+    std::cout << "Test 1 built without fail." << std::endl;
     return 0;
 }
--- test#3/main.cpp
+++ test#3/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 
+void my_god_function(int my_value)
+{
+    std::cout << "WE GOT A WARNING!" << std::endl;
+}
+
+static int my_ufunc() { return 52; }
+
 int main()
 {
-    std::cout << "Test 3 built without fail." << std::endl;
+    std::cout << "Test 3 built without fail!" << '\n';
+    std::cout << "This program should've generated warnings." << '\n';
+
+    my_god_function(72);
+
     return 0;
 }
