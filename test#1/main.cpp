a5864fb20f1a55d88e74c84a7a6b7b9294ac95e8
WildCard65 <lollol222gg@gmail.com>
2020-09-03 17:44:10 -0400

    Fully test capturing.

--- test#1/main.cpp
+++ test#1/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
-lobster;
+void insaneError() {}
 
 int main()
 {
     std::cout << "Test 1 built without fail." << std::endl;
+    insaneError(42);
     return 0;
 }
+
+EXTREME;
